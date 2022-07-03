61f58f0c0060775b3a982ebc9a37033274621a99
abhilash gudavalli <saiabhilash148@gmail.com>
2022-07-03 10:15:11 +0530

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Sai Abhilash Gudavalli on 03/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
